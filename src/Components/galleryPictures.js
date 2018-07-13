4006fd001cdf835f7d8aa3036ec52e2bf535fef8
nikita <nbron86@gmail.com>
2018-07-13 21:39:20 +0200

    added galleryPictures component

---
+++ src/Components/galleryPictures.js
@@ -0,0 +1,19 @@
+import React, {
+    Component
+} from 'react'
+import './includes/newGalleries.css';
+import axios from 'axios';
+
+class galleryPictures extends Component {
+
+
+    render() {
+        return (
+            <div className = "cont" >
+
+            </div >
+        )
+    }
+}
+
+export default galleryPictures
